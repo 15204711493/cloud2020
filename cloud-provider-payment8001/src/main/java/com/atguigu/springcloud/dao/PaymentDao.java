be14e9291dc7af67aced619635315964f0d270e2
袁建强 <15204711493@163.com>
2021-07-07 23:26:02 +0800

    order 支付

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 @Mapper
 //@Repository
 public interface PaymentDao {
+
     public int create(Payment payment);
 
     public Payment getPaymentById(Long id);
