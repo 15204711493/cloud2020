ce30661c63f071de5f09d059562b9905d349ee30
15204711493 <15204711493@163.com>
2021-07-12 17:05:39 +0800

    consul

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
