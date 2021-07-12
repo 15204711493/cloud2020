9c3566eb59a44fb028cee555128a2f8eb5498186
袁建强 <15204711493@163.com>
2021-07-12 23:05:36 +0800

    ForEntity

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,11 +26,38 @@ public class OrderController {
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
-
     }
+
+
+    @GetMapping("/consumer/payment/postForEntity/create")
+    public CommonResult<Payment> create2(Payment payment){
+        ResponseEntity<CommonResult> resultResponseEntity =
+                restTemplate.postForEntity(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+        if(resultResponseEntity.getStatusCode().is2xxSuccessful()){
+            return resultResponseEntity.getBody();
+        }else {
+            return new CommonResult<>(444,"添加失败");
+        }
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable Long id){
+       ResponseEntity<CommonResult>  resultResponseEntity=
+               restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+       if(resultResponseEntity.getStatusCode().is2xxSuccessful()){
+           log.info("===================================");
+           log.info(resultResponseEntity.getStatusCode()+"\t"+resultResponseEntity.getHeaders());
+           return resultResponseEntity.getBody();
+       }else {
+           return new CommonResult<>(444,"操作失败");
+       }
+    }
+
+
 }
